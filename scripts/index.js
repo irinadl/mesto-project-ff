eea04ea75c063fe9941b40475ca069db1eb32a9c
Irina Delyagina <irina_dl@mail.ru>
2024-08-08 16:28:06 +0300

    первая версия

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+
+function addCard(cards, funct) {
+  cards.forEach(function (element) {
+    const cardElement = cardTemplate.cloneNode(true);
+
+    cardElement.querySelector(".card__image").src = element.link;
+    cardElement.querySelector(".card__image").alt = element.name;
+    cardElement.querySelector(".card__title").textContent = element.name;
+
+    funct(cardElement);
+
+    placesList.append(cardElement);
+  });
+}
+
+function removeCard(btn) {
+  const deleteButton = btn.querySelector(".card__delete-button");
+
+   deleteButton.addEventListener("click", () => {
+   deleteButton.parentElement.remove();
+  });
+}
+
+addCard(initialCards, removeCard);
