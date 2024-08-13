4d73ec5e28479cbb81b6b6b60829aae63e859372
Irina Delyagina <irina_dl@mail.ru>
2024-08-13 18:04:07 +0300

    revision 2.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,38 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector("#card-template").content.querySelector(".places__item");
+
 // @todo: DOM узлы
+
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
+function createCard(data, onDelete) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__image").src = data.link;
+  cardElement.querySelector(".card__image").alt = data.name;
+  cardElement.querySelector(".card__title").textContent = data.name;
+
+  deleteButton.addEventListener("click", onDelete);
+
+  return cardElement;
+}
+
 // @todo: Функция удаления карточки
+
+function handleDeleteCard(evt) {
+  evt.target.parentElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
 
-const placesList = document.querySelector(".places__list");
-const cardTemplate = document.querySelector("#card-template").content;
-
-function addCard(cards, funct) {
-  cards.forEach(function (element) {
-    const cardElement = cardTemplate.cloneNode(true);
-
-    cardElement.querySelector(".card__image").src = element.link;
-    cardElement.querySelector(".card__image").alt = element.name;
-    cardElement.querySelector(".card__title").textContent = element.name;
-
-    funct(cardElement);
-
-    placesList.append(cardElement);
-  });
+function renderCard(card) {
+  placesList.append(card);
 }
 
-function removeCard(btn) {
-  const deleteButton = btn.querySelector(".card__delete-button");
-
-   deleteButton.addEventListener("click", () => {
-   deleteButton.parentElement.remove();
-  });
-}
-
-addCard(initialCards, removeCard);
+initialCards.forEach((card) => {
+  renderCard(createCard(card, handleDeleteCard));
+});
