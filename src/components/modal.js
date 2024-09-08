779ecbb4dff381cc4b2add72f62c1a2e09083dd7
Irina Delyagina <irina_dl@mail.ru>
2024-09-08 22:10:18 +0300

    вторая версия

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,50 +1,25 @@
 
-  export function newPopupOpen(popup) {
+  export function openPopup(popup) {
     popup.classList.add("popup_is-opened");
     popup.classList.add('popup_is-animated');
     document.addEventListener('keydown', checkEscapeKey);
   }
 
-  export function newClosePopup(popup) {
+  export function closePopup(popup) {
     popup.classList.remove("popup_is-opened");
     document.removeEventListener('keydown', checkEscapeKey);
   }
 
-  export function cardPopupOpen(evt) {
-    const imagePopup = document.querySelector('.popup_type_image');
-    imagePopup.classList.add("popup_is-opened");
-    imagePopup.classList.add("popup_is-animated");
-    document.addEventListener('keydown', checkEscapeKey);
-    const image = imagePopup.querySelector('.popup__image');
-    const description = imagePopup.querySelector('.popup__caption');
-    image.src = evt.target.src;
-    image.alt = evt.target.alt;
-    description.textContent = evt.target.alt;
-  }
-
-
-      export function closeOnBackDropClick({ currentTarget, target }) {
+        export function closeOnBackDropClick({ currentTarget, target }) {
         const dialog = currentTarget
         const isClickedOnBackDrop = target === dialog
         if (isClickedOnBackDrop) {
-          dialog.classList.remove("popup_is-opened");
-          document.forms.editprofile.reset();
-          document.forms.newplace.reset();
+         closePopup(dialog);
         }
       }
 
-    
-  export function handleFormSubmit(evt) {
-    evt.preventDefault();
-    const editForm = document.forms.editprofile;
-    document.querySelector('.profile__title').textContent = editForm.elements.name.value;
-    document.querySelector('.profile__description').textContent = editForm.elements.description.value; 
-    document.querySelector('.popup_type_edit').classList.remove("popup_is-opened");
-    editForm.reset();
-  }    
-
   export function checkEscapeKey(evt) {
-    const popup = document.querySelector('.popup_is-opened');
-    if (evt.key === 'Escape') {        
-      popup.classList.remove("popup_is-opened");}
-  }
+    if (evt.key === 'Escape') {
+    const popup = document.querySelector('.popup_is-opened');        
+    closePopup(popup);}
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import './pages/index.css';
 import {initialCards} from './components/cards';
-import {createCard} from './components/card';
-import {handleDeleteCard} from './components/card';
-import {renderCard} from './components/card';
-import {cardLikeBtn} from './components/card';
-import {cardPopupOpen} from './components/modal';
-import {closeOnBackDropClick} from './components/modal';
-import {handleFormSubmit} from './components/modal';
-import {newPopupOpen} from './components/modal';
-import {newClosePopup} from './components/modal';
-import {handleCreateCard} from './components/card';
-
+import {createCard, handleDeleteCard, likeCard} from './components/card';
+import {openPopup, closePopup, closeOnBackDropClick, checkEscapeKey} from './components/modal';
 
 //Попапы и кнопки к ним
 const editProfileButton = document.querySelector(".profile__edit-button");
@@ -25,9 +16,29 @@ const addCardForm = document.forms.newplace;
 
 //  Функция создания карточки
 
+function renderInitialCards(card) {
+  const placesList = document.querySelector(".places__list");
+  placesList.append(card);
+}
+
 initialCards.forEach((card) => {
-  renderCard(createCard(card, handleDeleteCard, cardLikeBtn, cardPopupOpen));
+  renderInitialCards(createCard(card, handleDeleteCard, likeCard, openImgPopup));
 });
+
+function handleCreateCard(evt) {
+  evt.preventDefault();
+  const addCardForm = document.forms.newplace;
+  const placeName = addCardForm.elements.placename;
+  const link = addCardForm.elements.link;
+    const newCard = {
+    name: placeName.value,
+    link: link.value
+  }
+  document.querySelector(".places__list").prepend(createCard(newCard, handleDeleteCard, likeCard, openImgPopup));
+  document.querySelector('.popup_type_new-card').classList.remove("popup_is-opened"); 
+  addCardForm.reset()
+}
+
 
 //Ручное создание  карточки 
 
@@ -35,19 +46,30 @@ addCardForm.addEventListener('submit', handleCreateCard);
 
 //Открываем модальные окна
 
-editProfileButton.addEventListener('click', (evt) => {
-  newPopupOpen(popupEditProfile);
-  initialEditProfileData(editForm);  
+editProfileButton.addEventListener('click', () => {
+  openPopup(popupEditProfile);
+  fillEditProfileData(editForm);  
 })
 
-function initialEditProfileData(form) {
+function fillEditProfileData(form) {
   form.elements.name.value = document.querySelector('.profile__title').textContent;
   form.elements.description.value = document.querySelector('.profile__description').textContent;
 }
 
-addCardButton.addEventListener('click', (evt) => {
-  newPopupOpen(popupAddCard);  
+addCardButton.addEventListener('click', () => {
+  openPopup(popupAddCard);
+  addCardForm.reset();  
  })
+
+ export function openImgPopup(data) {
+  openPopup(imagePopup);
+  document.addEventListener('keydown', checkEscapeKey);
+  const image = imagePopup.querySelector('.popup__image');
+  const description = imagePopup.querySelector('.popup__caption');
+  image.src = data.link;
+  image.alt = data.name;
+  description.textContent = data.name;
+}
 
 //Закрываем модальные окна
 
@@ -56,19 +78,24 @@ popupAddCard.addEventListener('click', closeOnBackDropClick);
 imagePopup.addEventListener('click', closeOnBackDropClick);
 
 popupEditProfile.querySelector('.popup__close').addEventListener('click',() => {
-  newClosePopup(popupEditProfile);
-  editForm.reset();
+  closePopup(popupEditProfile);  
 })
 
 popupAddCard.querySelector('.popup__close').addEventListener('click',() => {
-  newClosePopup(popupAddCard);
-  addCardForm.reset();
-})
+  closePopup(popupAddCard);
+ })
 
 imagePopup.querySelector('.popup__close').addEventListener('click',() => {
-  newClosePopup(imagePopup);
+  closePopup(imagePopup);
 })
 
 //Редактируем профиль
 
-editForm.addEventListener('submit', handleFormSubmit); 
+ function submitEditProdileForm(evt) {
+  evt.preventDefault();
+  document.querySelector('.profile__title').textContent = editForm.elements.name.value;
+  document.querySelector('.profile__description').textContent = editForm.elements.description.value; 
+  closePopup(popupEditProfile);  
+}   
+
+editForm.addEventListener('submit', submitEditProdileForm); 
